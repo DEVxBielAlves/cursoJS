10af7704288362e6a6843c686a4e734afb9cefb7
DEVXKowaski <gabriel.alves@megavalelog.com.br>
2025-08-13 22:59:37 -0300

    parei na aula 12

---
+++ secao2/aula11/comentarios.js
@@ -0,0 +1,14 @@
+// Comentário
+console.log("Hello World"); // Comentário de uma linha
+console.log("Linha 1");
+
+/* 
+Comentário de múltiplas linhas
+*/
+
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+console.log("Linha 5");
+console.log("Linha 6");
+console.log("Linha 7");
